cfdbf69f1160ebe79334c408c1a099cc1f8d7f21
John Holash <john.holash@ucalgary.ca>
2023-02-14 10:47:08 -0700

    added simply python files

---
+++ first.py
@@ -0,0 +1,5 @@
+# This is my first python program
+print ('what is your name ?')
+myName = input()
+print ('it is good to meet you , ' + myName)
+print ('the length of your name is :', (len(myName)))
